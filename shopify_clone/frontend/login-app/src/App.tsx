ecf3eae0ad8928679df5655f0b245bd7d3e44deb
calicomills <jishnuck26@gmail.com>
2024-04-15 22:33:55 +0530

    bitcask

---
+++ shopify_clone/frontend/login-app/src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import React from 'react';
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Shopify clone</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
